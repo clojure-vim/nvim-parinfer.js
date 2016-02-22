e9519fafdbb9822ae8b70aaf01dfebfd56c9c547
David Whittington <djwhitt@gmail.com>
2016-02-21 20:33:51 -0600

    Use standard Clojurescript indentation

--- src/nvim_parinfer/main.cljs
+++ src/nvim_parinfer/main.cljs
@@ -1,6 +1,6 @@
 (ns nvim-parinfer.main
   (:require
-   [parinfer :as parinfer]))
+    [parinfer :as parinfer]))
 
 (defn dbg
   [msg & args]
@@ -15,44 +15,44 @@
 
 (defn run-indent [current-text opts mode]
   (cond
-   (= "indent" mode)
-   (parinfer/indentMode current-text opts)
+    (= "indent" mode)
+    (parinfer/indentMode current-text opts)
 
-   (= "paren" mode)
-   (parinfer/parenMode current-text opts)))
+    (= "paren" mode)
+    (parinfer/parenMode current-text opts)))
 
 (defn format-lines [current-lines cursor-x cursor-line bufnum mode]
   (try
-   (let [opts #js {"cursorX" (dec cursor-x) "cursorLine" (dec cursor-line)}
-         current-text (.join current-lines "\n")
-         result (run-indent current-text opts mode)
-         new-text (.-text result)]
+    (let [opts #js {"cursorX" (dec cursor-x) "cursorLine" (dec cursor-line)}
+          current-text (.join current-lines "\n")
+          result (run-indent current-text opts mode)
+          new-text (.-text result)]
 
-    (when (not= current-text new-text)
-     (split-lines new-text)))
+      (when (not= current-text new-text)
+        (split-lines new-text)))
 
-   (catch :default e
-     (dbg "EXCEPTION" e e.stack))))
+    (catch :default e
+      (dbg "EXCEPTION" e e.stack))))
 
 (defn parinfer-indent
   [nvim args [[_ cursor-line cursor-x _] bufnum lines mode] nvim-callback]
   (let [start (js/Date.)]
     (if-let [new-lines (format-lines lines cursor-x cursor-line bufnum mode)]
       (do
-       #_(js/debug "c" (- (.getTime (js/Date.)) (.getTime start)))
-       (nvim-callback nil (clj->js new-lines)))
+        #_(js/debug "c" (- (.getTime (js/Date.)) (.getTime start)))
+        (nvim-callback nil (clj->js new-lines)))
       (do
-       #_(js/debug "n" (- (.getTime (js/Date.)) (.getTime start)))
-       (nvim-callback nil (clj->js []))))))
+        #_(js/debug "n" (- (.getTime (js/Date.)) (.getTime start)))
+        (nvim-callback nil (clj->js []))))))
 
 (defn -main []
   (try
-   (when (exists? js/plugin)
-     (js/debug "hello parinfer")
-     (.functionSync js/plugin "ParinferIndent"
-                    #js {:eval "[getpos('.'), bufnr('.'), getline(1,line('$')), g:parinfer_mode]"}
-                    parinfer-indent))
-   (catch :default e
-     (dbg "main exception" e e.stack))))
+    (when (exists? js/plugin)
+      (js/debug "hello parinfer")
+      (.functionSync js/plugin "ParinferIndent"
+                     #js {:eval "[getpos('.'), bufnr('.'), getline(1,line('$')), g:parinfer_mode]"}
+                     parinfer-indent))
+    (catch :default e
+      (dbg "main exception" e e.stack))))
 
 (set! *main-cli-fn* -main)
